17f98fa89837af835757614395984003fe9b7c87
Rushikesh Khupse <rushikhupse26@gmail.com>
2024-10-04 19:40:07 +0530

    Frontend completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+
+// export default App;
+
+import React from 'react';
+import axios from 'axios';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { CssBaseline } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import LandingPage from './Components/LandingPage';
+import Dashboard from './Components/Dashboard';
+import Grievance from './Components/Grievance';
+import AddAPO from './Components/AddAPO';
+import ComplaintForm from './Components/ComplaintForm';
+import APOLoginForm from './Components/APOLoginForm'; // Import APOLoginForm
+import APODashboard from './Components/APODashboard';
+import ErrorBoundary from './Components/ErrorBoundary';
+import './styles.css';
+
+// API instance for making requests
+const api = axios.create({
+  baseURL: 'http://localhost:8080/api',
+});
+
+// Theme configuration
+const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+    background: {
+      default: '#ffffff',
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ErrorBoundary>
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <Router>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/apo-login" element={<APOLoginForm />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/grievance" element={<Grievance />} />
+            <Route path="/add-apo" element={<AddAPO />} />
+            <Route path="/complaint-form" element={<ComplaintForm />} />
+            <Route path="/apo-dashboard" element={<APODashboard />} /> {/* Route for APO Dashboard */}
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </ErrorBoundary>
   );
 }
 
